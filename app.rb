4cdf7fc4765c779d99bd8594aaa25ea608f642e9
Albrecht Oster <albrecht@oster.online>
2019-02-02 16:28:38 +0100

    add support for multiple hosts with separate prefixes

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'active_support/core_ext/object/blank'
 require 'yaml'
+require 'yaml/store'
 require 'date'
 
 def config_file_content(filename)
@@ -14,12 +15,22 @@ def development?
   Sinatra::Base.development?
 end
 
-config = YAML.safe_load(config_file_content('config.yml'))
-last_serial = { date: nil, counter: 0 }
+def addr_for_host(hostname, type = :v6)
+  settings.addresses[type][hostname] || (type == :v6 ? '::' : '127.0.0.1')
+end
+
+configure do
+  config = YAML.safe_load(config_file_content('config.yml'))
+  set :addresses, YAML::Store.new('config/addresses.yml')
+  set :auth_tokens, config['auth_tokens']
+  set :target_zone, config['target_zone']
+  set :last_serial, date: nil, counter: 0
+end
 
 put '/hostnames' do
-  return status 500 if config['auth_token'].blank?
-  return status 401 if request.env['HTTP_X_AUTHORIZATION'] != config['auth_token']
+  return status 500 unless settings.auth_tokens&.any?
+  hostname = settings.auth_tokens.key(request.env['HTTP_X_AUTHORIZATION'])&.to_sym
+  return status 401 unless hostname.present?
 
   begin
     data = JSON.parse(request.body.read)
@@ -29,26 +40,31 @@ put '/hostnames' do
 
   return status 422 if data['prefix'].blank? || data['ipv4'].blank?
 
-  if last_serial[:date] == Date.today
-    last_serial[:counter] += 1
+  if settings.last_serial[:date] == Date.today
+    settings.last_serial[:counter] += 1
   else
-    last_serial[:date] = Date.today
-    last_serial[:counter] = 0
+    settings.last_serial[:date] = Date.today
+    settings.last_serial[:counter] = 0
   end
 
-  counter = format('%.2d', last_serial[:counter])
+  counter = format('%.2d', settings.last_serial[:counter])
   serial = "#{Time.now.strftime('%Y%m%d')}#{counter}"
-  prefix = data['prefix']
-  ipv4 = data['ipv4']
 
-  return status 500 if config['target_zone'].blank?
-  zonefile = ERB.new(config_file_content('zonefile.zone.erb'))
-  target = development? ? '/tmp' : '/usr/local/etc/nsd/zones'
-  target += "/#{config['target_zone']}.zone"
-  return status 500 unless development? || File.exist?(target)
-  File.write(target, zonefile.result(binding))
+  settings.addresses.transaction do
+    settings.addresses[:v6] ||= {}
+    settings.addresses[:v4] ||= {}
+    settings.addresses[:v6][hostname] = data['prefix']
+    settings.addresses[:v4][hostname] = data['ipv4']
 
-  return 500 unless development? || system("nsd-control reload '#{config['target_zone']}'")
+    return status 500 if settings.target_zone.blank?
+    zonefile = ERB.new(config_file_content('zonefile.zone.erb'))
+    target = development? ? '/tmp' : '/usr/local/etc/nsd/zones'
+    target += "/#{settings.target_zone}.zone"
+    return status 500 unless development? || File.exist?(target)
+    File.write(target, zonefile.result(binding))
+  end
+
+  return 500 unless development? || system("nsd-control reload '#{settings.target_zone}'")
 
   status 204
 end
