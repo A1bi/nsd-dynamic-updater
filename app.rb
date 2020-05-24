71879bda17662893928573df879af6212cfa9ffe
Albrecht Oster <albrecht@oster.online>
2020-05-24 17:32:48 +0200

    add convenience method for generating complete address records
    add sample zonefile

--- app.rb
+++ app.rb
@@ -32,7 +32,15 @@ end
 def addr_for_host(host, address_id, suffix: '')
   address = settings.addresses[host.to_s][address_id.to_s]
   # this will raise an exception for invalid resulting addresses
-  IPAddr.new("#{address}#{suffix}").to_s
+  IPAddr.new("#{address}#{suffix}")
+end
+
+def address_record(name, host, address_id, suffix: '')
+  address = addr_for_host(host, address_id, suffix: suffix)
+  address ||= '::1'
+  record_type = address.ipv4? ? 'A' : 'AAAA'
+
+  "#{name} IN #{record_type} #{address}"
 end
 
 configure do
